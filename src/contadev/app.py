4103736c1ec5e5183fe07ebaaa45c55573c7ea4b
John Zhou <john-xyz@outlook.com>
2025-11-25 18:17:39 -0600

    Overhaul

--- src/contadev/app.py
+++ src/contadev/app.py
@@ -1,19 +1,13 @@
-"""
-My first application
-"""
-
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import asyncio
-
 
 class OptionScrollApp(toga.App):
     def startup(self):
         self.main_window = toga.MainWindow(title=self.formal_name)
 
+        # Scrollable tab
         scroll_box = toga.Box(style=Pack(direction=COLUMN, margin=8))
-
         for i in range(1, 51):
             row = toga.Box(style=Pack(direction=ROW, align_items='center', margin_bottom=4))
             lbl = toga.Label(f"Item {i}", style=Pack(width=120))
@@ -21,44 +15,40 @@ class OptionScrollApp(toga.App):
             row.add(lbl)
             row.add(btn)
             scroll_box.add(row)
-
         self.scroll_container = toga.ScrollContainer(content=scroll_box, flex=1)
 
+        # Simple tab
         other_box = toga.Box(style=Pack(direction=COLUMN, margin=8))
         other_box.add(toga.Label("This is another tab."))
         other_box.add(toga.Button("Do something", on_press=self._on_do_something))
 
+        # Web tab
+        webview = toga.WebView(url="https://google.com")
+
+        # Option container
         self.option_container = toga.OptionContainer(
             content=[
-               ("Scrollable", toga.ScrollContainer(content=toga.Box(
-                   children=[self.scroll_container, toga.Label("Additional Text")], direction=COLUMN
+                ("Scrollable", toga.ScrollContainer(content=toga.Box(
+                    children=[self.scroll_container, toga.Label("Additional Text")], direction=COLUMN
                 ))),
-                # ("Scrollable?", toga.ScrollContainer(content=toga.Box(
-                #     children=[
-                #         toga.Box(direction=COLUMN, flex=1),
-                #         toga.Label("Additional Text")
-                #     ],
-                #     flex=1,
-                #     direction=COLUMN
-                # ))),
-#                ("Scrollable", self.scroll_container),
                 ("Simple", other_box),
+                ("Web", webview),  # New Web tab
                 ("Option", toga.OptionContainer(content=[
                     ("Hello!", toga.Box(children=[toga.Label("World!")]))
-                                                         ]))
+                ]))
             ],
             style=Pack(flex=1),
         )
 
         self.main_window.content = self.option_container
         self.main_window.show()
-        print(self.main_window._impl.container)
 
     def _on_item_pressed(self, widget):
-        print("a")
+        print("Item pressed")
 
     def _on_do_something(self, widget):
         print("Do something pressed")
+
 
 def main():
     return OptionScrollApp()
